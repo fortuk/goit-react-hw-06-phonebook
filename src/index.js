a422221784697fb671c78ab14c63ccd68098ba36
Алексей <fortuk22@rambler.ru>
2022-06-29 23:00:42 +0300

    root

--- src/index.js
+++ src/index.js
@@ -7,19 +7,14 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import store from './redux/store'
 
-ReactDOM.render( <
-    React.StrictMode >
-    <
-    Provider store = { store.store } >
-    <
-    PersistGate loading = { null }
-    persistor = { store.persistor } >
-    <
-    App / >
-    <
-    /PersistGate> <
-    /Provider> <
-    /React.StrictMode>,
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store.store} >
+            <PersistGate loading={null} persistor={store.persistor} >
+                <App />
+            </PersistGate>
+        </Provider>
+    </React.StrictMode >,
     document.getElementById('root'),
 );
 // If you want to start measuring performance in your app, pass a function
