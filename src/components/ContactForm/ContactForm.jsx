8273468c0e6021a2475a4405c03dbee760ad7034
Алексей <fortuk22@rambler.ru>
2022-06-29 23:51:58 +0300

    hook

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,82 +1,98 @@
-import React, { useState } from 'react';
-import shortid from 'shortid';
+import { useState } from "react";
+import s from './ContactForm.module.css'
+import { connect } from 'react-redux';
+import contactsActions from '../../redux/contacts/contacts-actions';
 import PropTypes from "prop-types";
-import s from './ContactForm.module.css';
 
-function ContactForm({ onSubmit }) {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
 
-  const handleChange = event => {
-    const { name, value } = event.currentTarget;
-    switch (name) {
-      case 'name':
-        setName(value);
-        break;
 
-      case 'number':
-        setNumber(value);
-        break;
+function ContactForm({ onSubmit, contacts }) {
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
 
-      default:
-        return;
-    }
-  };
+    const handleChange = event => {
+        const { name, value } = event.currentTarget;
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    onSubmit({ name, number });
-    reset();
-  };
+        switch (name) {
+            case 'name':
+                setName(value);
+                break;
 
-  const reset = () => {
-    setName('');
-    setNumber('');
-  };
+            case 'number':
+                setNumber(value);
+                break;
 
-  const nameId = shortid.generate();
-  const numberId = shortid.generate();
+            default:
+                return;
+        }
+    };
 
-  return (
-    <form onSubmit={handleSubmit} className={s.form}>
-      <label className={s.label} htmlFor={nameId}>
-        Name
-        <input
-          className={s.input}
-          type="text"
-          name="name"
-          id={nameId}
-          value={name}
-          onChange={handleChange}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-      </label>
-      <label className={s.label} htmlFor={numberId}>
-        Number
-        <input
-          className={s.input}
-          type="tel"
-          name="number"
-          id={numberId}
-          value={number}
-          onChange={handleChange}
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        />
-      </label>
+    const handleSubmit = event => {
+        event.preventDefault();
 
-      <button className={s.button} type="submit">
-        Add contact
-      </button>
-    </form>
-  );
+        const options = { name, number };
+
+        if (
+            contacts.find(
+                contact => name.toLowerCase() === contact.name.toLowerCase(),
+            )
+        ) {
+            alert(`${name} is already in contacts`);
+        } else {
+            onSubmit(options);
+        }
+
+        reset();
+    };
+
+    const reset = () => {
+        setName('');
+        setNumber('');
+    };
+
+
+
+    return (
+
+        <form onSubmit={handleSubmit} className={s.form}>
+            <label className={s.label}> Name
+                <input className={s.input}
+                    onChange={handleChange}
+                    value={name}
+                    type="text"
+                    name="name"
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+                    required
+                />
+            </label>
+            <label className={s.label}> Number
+                <input className={s.input}
+                    onChange={handleChange}
+                    value={number}
+                    type="tel"
+                    name="number"
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+                    required
+                />
+            </label>
+            <button type="Submit" className={s.button}>Add</button>
+
+        </form>
+    )
 }
-
 ContactForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    contacts: PropTypes.string.isRequired,
 };
 
-export default ContactForm;
+const mapStateToProps = ({ contacts: { items } }) => ({
+    contacts: items,
+});
+
+const mapDispatchToProps = dispatch => ({
+    onSubmit: contact => dispatch(contactsActions.addContact(contact)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ContactForm);
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,23 +1,22 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import s from './Filter.module.css';
+import s from './Filter.module.css'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux';
+import contactsActions from '../../redux/contacts/contacts-actions';
 
 const Filter = ({ inputValue, onChange }) => (
-  <label className={s.label}>
-    Find contacts by name
-    <input
-      className={s.input}
-      type="text"
-      name="filter"
-      value={inputValue}
-      onChange={onChange}
-    />
-  </label>
-);
-
+    <label className={s.label}>
+        <input type="text" value={inputValue} onChange={onChange} className={s.input} />
+    </label>
+)
 Filter.propTypes = {
-  inputValue: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
+    inputValue: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
 };
+const mapStateToProps = state => ({
+    inputValue: state.contacts.filter,
+});
+const mapDispatchToProps = dispatch => ({
+    onChange: event => dispatch(contactsActions.changeFilter(event.target.value)),
+});
 
-export default Filter;
+export default connect(mapStateToProps, mapDispatchToProps)(Filter);
